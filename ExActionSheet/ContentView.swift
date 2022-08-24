d5b8217b1ceac7ec734805081a17f086db75687b
jake <palatable7@naver.com>
2022-08-24 23:52:20 +0900

    Implement

--- ExActionSheet/ContentView.swift
+++ ExActionSheet/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var isShowing = false
+  @State var isActionSheetShowing = false
+  
+  var body: some View {
+    VStack {
+      Button("Alert") {
+        isShowing = true
+      }
+      .alert("title", isPresented: $isShowing) {
+        Button("OK", role: .destructive) { print("tap ok") }
+        Button("cancel", role: .cancel) { print("tap cancel") }
+      }
+      Divider()
+      Button("ActionSheet") {
+        isActionSheetShowing = true
+      }
+      .confirmationDialog("title", isPresented: $isActionSheetShowing) {
+        Button("OK", role: .destructive) { print("tap ok") }
+        Button("cancel", role: .cancel) { print("tap cancel") }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
