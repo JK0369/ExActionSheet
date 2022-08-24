42a3101436bcc860109def66f4c194aabc7fda39
jake <palatable7@naver.com>
2022-08-24 23:28:29 +0900

    Initial Commit

---
+++ ExActionSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExActionSheet
+//
+//  Created by 김종권 on 2022/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExActionSheet/ExActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExActionSheetApp.swift
+//  ExActionSheet
+//
+//  Created by 김종권 on 2022/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ExActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
